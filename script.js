4af63e5b383d3194c48ae43ef134d2f6c2575cf0
Nestor Villon <46238008@continental.edu.pe>
2025-05-04 23:25:48 -0500

    Agregado: resultado final y botón para reiniciar examen

--- script.js
+++ script.js
@@ -19,16 +19,16 @@ const bancoPreguntas = [
     ],
     correcta: 0
   },
-    {
-      pregunta: "Qual è il primo parametro da rilevare, in caso di dispnea?",
-      opciones: [
-        "La difficoltà respiratoria",
-        "L'utilizzo dei muscoli accessori",
-        "La difficoltà ad effettuare attività",
-        "Il colorito cutaneo e la presenza di cianosi o pallore"
-      ],
+  {
+    pregunta: "Qual è il primo parametro da rilevare, in caso di dispnea?",
+    opciones: [
+      "La difficoltà respiratoria",
+      "L'utilizzo dei muscoli accessori",
+      "La difficoltà ad effettuare attività",
+      "Il colorito cutaneo e la presenza di cianosi o pallore"
+     ],
       correcta: 0
-    },
+  },
     {
       pregunta: "I compiti forniti dall'infermiere devono essere eseguiti sempre e comunque dall'OSS?",
       opciones: [
@@ -68,7 +68,257 @@ const bancoPreguntas = [
         "L'Oss inclina lo schienale della carrozzina"
       ],
       correcta: 0
-    }
+    },
+      {
+        pregunta: "Quale tra queste azioni NON deve compiere l'Oss nel rifacimento del letto?",
+        opciones: [
+          "Scuotere la biancheria usata con vigore",
+          "Maneggiare il meno possibile la biancheria usata",
+          "Piegare con cura la biancheria pulita",
+          "Posizionare il campanello di chiamata a portata della persona"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Su cosa si basa il modello di lavoro \"per équipe\"?",
+        opciones: [
+          "Distribuzione delle responsabilità in funzione delle competenze della condivisione di obiettivi e decisioni",
+          "Turnazione del lavoro",
+          "Tendenziale deresponsabilizzazione dei membri dell'équipe",
+          "Ripetitività delle mansioni affidate ai membri dell'équipe"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Come avviene il confezionamento, nel caso di biancheria infetta?",
+        opciones: [
+          "Separatamente attraverso un sacco interno idrosolubile e un sacco esterno in polietilene",
+          "Insieme a tutta la biancheria piana",
+          "Nessuna delle altre risposte è corretta",
+          "Separatamente attraverso un sacco in polietilene"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Quale affermazione NON è corretta in riferimento al lavaggio delle mani.",
+        opciones: [
+          "Non è obbligatorio prima delle manovre assistenziali",
+          "Può essere effettuato anche senza l'utilizzo di acqua corrente",
+          "È la misura più importante per la prevenzione delle infezioni ospedaliere",
+          "Può essere sociale, antisettico, chirurgico"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "In quale dei seguenti casi, un paziente emette radiazioni ionizzanti strumentalmente?",
+        opciones: [
+          "Quando ha effettuato un esame scintigrafico",
+          "Quando ha effettuato un esame TAC",
+          "Quando ha effettuato una RMN",
+          "Quando ha effettuato un'ecografia"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "(Raccolta di campioni di urine nelle ventiquattro ore) L'urina della prima minzione:",
+        opciones: [
+          "non deve essere tenuta",
+          "a discrezione della persona",
+          "deve essere tenuta",
+          "può essere tenuta"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "non è considerato un elemento che permette di distinguere una confezione sterile.",
+        opciones: [
+          "La presenza di indicatori di viraggio",
+          "Il colore del materiale con il quale viene realizzato il confezionamento",
+          "L'integrità della confezione",
+          "La descrizione del contenuto, la data di preparazione e di scadenza"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Quando va eseguita l'igiene del cavo orale?",
+        opciones: [
+          "Dopo ogni pasto o almeno tre volte al giorno",
+          "Una volta al giorno",
+          "Due volte al giorno",
+          "Tre volte al giorno"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Come si esegue la detersione di una ulcera da pressione?",
+        opciones: [
+          "Va eseguita con soluzione fisiologica",
+          "Va eseguita con antisettici a basa di iodio",
+          "Va eseguita con soluzioni a base di ipoclorito di sodio",
+          "Va eseguita con clorexidina gluconato"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Quale tra i seguenti interventi NON va attuato davanti ad una persona con improvviso malore?",
+        opciones: [
+          "Posizionare a letto e aspettare che si riprenda",
+          "Controllare il respiro e circolo",
+          "Verificare lo stato di coscienza",
+          "Distendere la persona in posizione antishock"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Quale tra i seguenti interventi NON va attuato in caso di caduta di un paziente?",
+        opciones: [
+          "Posizionare dei cuscini sotto la testa e/o gli arti per diminuire il dolore e farlo stare più comodo",
+          "Mettere in posizione di sicurezza se incosciente",
+          "Rilevare lo stato di coscienza e la presenza di circolo",
+          "Lasciare la persona a terra fino all'arrivo di infermiere o medico"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Quale tra i seguenti parametri NON va controllato in un paziente in terapia diuretica?",
+        opciones: [
+          "La frequenza cardiaca",
+          "L'assunzione di liquidi nelle ventiquattro ore",
+          "La presenza di edemi",
+          "La diuresi nelle ventiquattro ore"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Come deve essere smaltito un bendaggio usato, senza pomata e non contaminato?",
+        opciones: [
+          "Come rifiuto sanitario non pericoloso assimilato agli urbani",
+          "Come rifiuto sanitario che prevede particolari sistemi di gestione",
+          "Come rifiuto sanitario pericoloso a rischio infettivo",
+          "Come rifiuto sanitario pericoloso a rischio non infettivo"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "non è un principio di corretta sanificazione.",
+        opciones: [
+          "Disinfettare quotidianamente il pavimento delle stanze di degenza",
+          "Scegliere detergenti differenziati in base allo sporco",
+          "Spolverare con un panno pulito e umido",
+          "Adoperare panni monouso differenziati"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Quale posizione è più opportuna da far assumere al paziente per la somministrazione dell'enteroclisma?",
+        opciones: [
+          "La posizione decubito laterale sinistro con ginocchio destro flesso",
+          "La posizione prona",
+          "La posizione di Cromwell",
+          "La posizione decubito destro con ginocchio flesso"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Qual è la corretta modalità di raccolta di un campione di urina per urinocoltura?",
+        opciones: [
+          "Pulizia dei genitali, mani lavate, contenitore sterile, mitto intermedio",
+          "Pulizia dei genitali, mani lavate, contenitore pulito, mitto intermedio",
+          "Pulizia dei genitali, mani lavate, contenitore non sterile, mitto iniziale",
+          "Pulizia dei genitali, digiuno, contenitore pulito, mitto intermedio"
+        ],
+        correcta: 0
+      },
+      {
+        pregunta: "Nel caso di contatto accidentale con materiale biologico, l'Oss deve effettuare il:",
+        opciones: [
+          "lavaggio antisettico",
+          "nessun lavaggio particolare",
+          "lavaggio sociale",
+          "lavaggio di routine"
+        ],
+        correcta: 0
+      },
+        {
+          pregunta: "Quaffermazione NON è corretta riguardo l'assistenza nel paziente portatore di catetere vescicale.",
+          opciones: [
+            "Il sacchetto in mancanza di supporto può stare a terra",
+            "Il sacchetto deve essere sistemato nell'apposito supporto",
+            "Il sistema di raccolta deve essere sterile",
+            "Il sacchetto deve stare in basso rispetto al bacino"
+          ],
+          correcta: 0
+        },
+        {
+          pregunta: "In che modo l'Oss deve sistemare il piano, dovendo posizionare un paziente nella posizione laterale destra?",
+          opciones: [
+            "L'Oss deve sistemare il piano in posizione orizzontale",
+            "L'Oss deve sistemare il piano a 45 gradi",
+            "L'Oss deve sistemare il piano a 90 gradi",
+            "L'Oss deve sistemare il piano a 30 gradi"
+          ],
+          correcta: 0
+        },
+        {
+          pregunta: "Nell'assistenza alla persona disfagica che cosa è importante?",
+          opciones: [
+            "Rispettare i tempi di somministrazione del pasto",
+            "Posizionare la persona con il capo leggermente reclinato indietro",
+            "Somministrare una dieta a doppia consistenza",
+            "Adoperare la cannuccia per la somministrazione delle bevande"
+          ],
+          correcta: 0
+        },
+        {
+          pregunta: "Come deve esere posizionato il paziente che abbia un episodio ipotensivo?",
+          opciones: [
+            "Deve essere posizionato disteso supino con i piedi sollevati",
+            "Deve essere posizionato nella posizione di sicurezza",
+            "Deve essere posizionato disteso supino con capo sollevato",
+            "Deve essere posizionato nella posizione semiseduta"
+          ],
+          correcta: 0
+        },
+        {
+          pregunta: "Durante l'assistenza di una persona affetta da disfagia di grado moderato è importante:",
+          opciones: [
+            "rispettare i tempi nella somministrazione del pasto",
+            "posizionare la persona con il capo leggermente reclinato indietro",
+            "somministrare una dieta a doppia consistenza",
+            "adoperare la cannuccia per la somministrazione delle bevande"
+          ],
+          correcta: 0
+        },
+        {
+          pregunta: "Assistendo una persona con diarrea è importante prevedere:",
+          opciones: [
+            "l'integrazione di liquidi",
+            "il controllo della cute",
+            "l'introduzione di alimenti ricchi di fibre",
+            "l'integrazione di alimenti ricchi di zuccheri"
+          ],
+          correcta: 0
+        },
+        {
+          pregunta: "Nella gestione di un paziente con catetere vescicale per ridurre il rischio di infezione delle vie urinarie è importante:",
+          opciones: [
+            "favorire l'idratazione",
+            "sostituire la sacca di raccolta delle urine una volta ogni ventiquattro ore",
+            "igiene perineale accurata e quotidiana con disinfettante",
+            "appoggiare la sacca di raccolta di urina sopra il livello del materasso"
+          ],
+          correcta: 0
+        },
+        {
+          pregunta: "I bisogni del malato sono:",
+          opciones: [
+            "bisogni fisiologici, bisogni di stima e autorealizzazione, bisogni di sicurezza e bisogni di appartenenza",
+            "bisogni fisiologici (mangiare, dormire, respirare, muoversi, …) e bisogni di autorealizzazione",
+            "bisogni di sicurezza per reazione a paura, insicurezza, ansia e bisogni di autorealizzazione",
+            "bisogni di amore e di appartenenza e bisogni di autorealizzazione e bisogni fisiologici (mangiare, dormire, respirare, muoversi, …)"
+          ],
+          correcta: 0
+        },      
   // Agrega más preguntas aquí...
 ];
 
